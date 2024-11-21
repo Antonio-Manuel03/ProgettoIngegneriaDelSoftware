ac7fcb7b1253fdd9576a1b6216a0700c903bab42
Antonio Manuel Fedullo <fedulloantoniomanuel@gmail.com>
2024-11-21 18:00:19 +0100

    Prova commit

---
+++ src/main/java/it/unisa/diem/ids/IngegneriaDelSoftwareProgetto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package it.unisa.diem.ids;
+
+/**
+ *
+ * @author antoniofedullo
+ */
+public class IngegneriaDelSoftwareProgetto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
